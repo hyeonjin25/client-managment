1fa4aca32911bd426755b5d9d8ac3e983dc15a6b
sohyeonjin <shj41738866@gmail.com>
2020-11-18 03:06:37 +0900

    first commit

---
+++ src/Components/Customer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Customer extends Component {
+    render() {
+        return (
+            <div>
+      <h2>소현진</h2>
+      <p>121916</p>
+      <p>여자</p>
+      <p>대학생</p>
+    </div>
+        )
+    }
+}
+
+export default Customer
